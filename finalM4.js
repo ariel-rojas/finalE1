15263b13745dc485a0a65e09c065dd75efd865e0
Ariel Rojas <arirojas50@gmail.com>
2022-08-03 15:32:13 -0300

    preguntas armonicos

--- finalM4.js
+++ finalM4.js
@@ -11,7 +11,8 @@ const preguntas = [
     "Desarrollo de Taylor",
     "Probar que son equivalentes la siguientes afirmaciones: \n 1)f es derivable en a \n 2) u y v son diferenciables en a y cumplen las condiciones de Cauchy-Riemann",
     "Probar que si f= u+iv es holomorfa en A siendo u y v C2, entonces u y v son armónicas en A",
-    "si U es armónica en un abierto D, existe una funcion v armónica conjugada tal que f=u+iv sea holomorfa en D?"
+    "si U es armónica en un abierto D, existe una funcion v armónica conjugada tal que f=u+iv sea holomorfa en D?",
+    "probar que si f_n son continuas en A y convergen uniformemente a f, entonces f es continua en A"
 ]
 
 const pregunta = document.querySelector('.pregunta');
