95fed3a8fc27f4b668d65a36b490a3efc1685562
Ariel Rojas <101291904+ariel-rojas@users.noreply.github.com>
2022-12-14 18:07:22 -0300

    Update finalM4.js

--- finalM4.js
+++ finalM4.js
@@ -1,7 +1,7 @@
 const preguntas = [
     "Deducir los teoremas de Bernoulli",
     "Deducir el teorema del momento",
-    "Deducir el teorema de Kelvin. Particularizar en flujos barotrópicos y baroclínicos. (ejemplo de la playa de dia y de noche)"
+    "Deducir el teorema de Kelvin"
 ]
 
 const pregunta = document.querySelector('.pregunta');
