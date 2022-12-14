fe696de9c049801879ee76d2c23370e550fc7483
Ariel Rojas <101291904+ariel-rojas@users.noreply.github.com>
2022-12-14 15:54:49 -0300

    actualizo a preguntas e1

--- finalM4.js
+++ finalM4.js
@@ -1,19 +1,7 @@
 const preguntas = [
-    "Condiciones de Cauchy-Riemmann",
-    "Fórmula de Cauchy de la derivada",
-    "Transformada de Fourier",
-    "Teorema de los residuos",
-    "Series de Fourier",
-    "Fórmula de Cauchy",
-    "Clasificación de singularidades",
-    "Transformada de Laplace",
-    "Desarrollo de Laurent",
-    "Desarrollo de Taylor",
-    "Probar que son equivalentes la siguientes afirmaciones: \n 1)f es derivable en a \n 2) u y v son diferenciables en a y cumplen las condiciones de Cauchy-Riemann",
-    "Probar que si f= u+iv es holomorfa en A siendo u y v C2, entonces u y v son armónicas en A",
-    "si U es armónica en un abierto D, existe una funcion v armónica conjugada tal que f=u+iv sea holomorfa en D?",
-    "probar que si f_n son continuas en A y convergen uniformemente a f, entonces f es continua en A",
-    "Serie de potencias"
+    "Deducir los teoremas de Bernoulli"
+    "Deducir el teorema del momento",
+    "Deducir el teorema de Kelvin. Particularizar en flujos barotrópicos y baroclínicos. (ejemplo de la playa de dia y de noche)",
 ]
 
 const pregunta = document.querySelector('.pregunta');
